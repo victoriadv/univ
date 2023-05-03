ae779b0e9a1fec7b1d1930462671212060ebd13a
victoriadv <72457978+victoriadv@users.noreply.github.com>
2023-05-03 19:50:59 +0300

    testNG lab

--- src/test/java/com/beginsecure/domain/DataSorterTest.java
+++ src/test/java/com/beginsecure/domain/DataSorterTest.java
@@ -1,53 +1,46 @@
 package com.beginsecure.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.junit.jupiter.api.Assertions.assertNull;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.testng.Assert.assertThrows;
 
-class DataSorterTest {
+public class DataSorterTest {
 
     private DataSorter dataSorter;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeMethod
+    public void setUp() {
         dataSorter = new DataSorter();
     }
 
-    @Test
-    void removeDublicate() {
+    @Test(groups = "GroupA")
+    public void removeDublicate() {
         String input = "Тест 123 Hello world";
         String expectedResult = "123 Hello Тест world";
         String result = dataSorter.removeDublicate(input);
         assertThat(result, equalTo(expectedResult));
     }
 
-    @Test
-    void testRemoveVowelsThrowsException() {
+    @Test(groups = "GroupA")
+    public void testRemoveVowelsThrowsException() {
         assertThrows(IllegalArgumentException.class, () -> dataSorter.removeVowels(null));
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "доброго, дбрг",
-            "morning, mrnng",
-            "Have, Hv"
-    })
-    void removeVowels(String input, String expectedResult) {
+    @DataProvider(name = "removeVowelsDataProvider")
+    public Object[][] removeVowelsDataProvider() {
+        return new Object[][]{
+                {"доброго", "дбрг"},
+                {"morning", "mrnng"},
+                {"Have", "Hv"}
+        };
+    }
+
+    @Test(groups = "GroupB", dataProvider = "removeVowelsDataProvider")
+    public void removeVowels(String input, String expectedResult) {
         String result = dataSorter.removeVowels(input);
         assertThat(result, equalTo(expectedResult));
     }
-
-    @Test
-    void testRemoveVowelsHandlesNull() {
-        DataSorter dataSorter = new DataSorter();
-        assertThrows(IllegalArgumentException.class, () -> dataSorter.removeVowels(null));
-    }
-
 }
-
--- src/test/java/com/beginsecure/domain/TextParserTest.java
+++ src/test/java/com/beginsecure/domain/TextParserTest.java
@@ -1,60 +1,62 @@
 package com.beginsecure.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.hamcrest.Matchers.startsWith;
-import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.endsWith;
 
-class TextParserTest {
+
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
+
+public class TextParserTest {
 
     private TextParser textParser;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeMethod
+    public void setUp() {
         textParser = new TextParser();
     }
 
-    @Test
-    void parseText() {
+    @Test(groups = "GroupA")
+    public void parseText() {
         List<String> input = Arrays.asList("Тест! 123", "Hello, world!");
         String expectedResult = "Тест 123 Hello world";
         String result = textParser.parseText(input);
         assertThat(result, equalTo(expectedResult));
     }
 
-    @Test
-    void parseTextStartsWith() {
+    @Test(groups = "GroupA")
+    public void parseTextStartsWith() {
         List<String> input = Arrays.asList("Тест! 123", "Hello, world!");
         String result = textParser.parseText(input);
         assertThat(result, startsWith("Тест"));
     }
 
-    @Test
-    void parseTextEndsWith() {
+    @Test(groups = "GroupB")
+    public void parseTextEndsWith() {
         List<String> input = Arrays.asList("Тест! 123", "Hello, world!");
         String result = textParser.parseText(input);
         assertThat(result, endsWith("world"));
     }
-    @Test
-    void parseTextContainsDigits() {
+
+    @Test(groups = "GroupB")
+    public void parseTextContainsDigits() {
         List<String> input = Arrays.asList("Тест! 123", "Hello, world!");
         String result = textParser.parseText(input);
         assertTrue(result.matches(".*\\d+.*"), "Result should contain digits");
     }
 
-    @Test
-    void parseTextDoesNotContainSpecialCharacters() {
+    @Test(groups = "GroupB")
+    public void parseTextDoesNotContainSpecialCharacters() {
         List<String> input = Arrays.asList("Тест! 123", "Hello, world!");
         String result = textParser.parseText(input);
         assertFalse(result.matches(".*[!@#$%^&*(),.?\":{}|<>].*"), "Result should not contain special characters");
     }
-
 }
+
